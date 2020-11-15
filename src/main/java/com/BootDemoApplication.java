0edb8803ebcaf723a47f7cbf7cab4ec80d8a96c9
zhangxiao <zhangxiao1@hatech.com.cn>
2020-11-15 21:06:55 +0800

    demo-准备

--- src/main/java/com/BootDemoApplication.java
+++ src/main/java/com/BootDemoApplication.java
@@ -1,25 +1,19 @@
 package com;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-import org.springframework.web.client.RestTemplate;
-
-import java.io.PrintStream;
 
 @EnableEurekaClient
+
 @SpringBootApplication
 public class BootDemoApplication {
 
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+//	@Bean
+//	@LoadBalanced
+//	public RestTemplate restTemplate() {
+//		return new RestTemplate();
+//	}
 
 
 	public static void main(String[] args) {
